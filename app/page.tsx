39aaa6979b701bc8724f54a878574b1c72f43a8d
지윤 <1yoouoo@gmail.com>
2024-06-05 22:28:35 +0900

    eslint 룰 설정

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="min-screen flex flex-col items-center justify-between p-24">
       <h1> CICD 테스트!</h1>
     </main>
-  );
+  )
 }
