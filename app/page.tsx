b8071bf095ea9422d44b30704cb9d036b9c5f0b0
지윤 <1yoouoo@gmail.com>
2024-06-06 11:19:13 +0900

    로드밸런서 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <main className="min-sc items-cer flex flex-col justify-between p-24">
-      <h1> ESLINT!</h1>
+      <h1>로드밸런서</h1>
     </main>
   )
 }
