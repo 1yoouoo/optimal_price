91a8f1267e5f13d44c1d79b55195ad6ed6954dab
지윤 <1yoouoo@gmail.com>
2024-06-06 11:34:26 +0900

    droplet

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-sc items-cer flex flex-col justify-between p-24">
-      <h1>로드밸런서</h1>
+      <h1>새로운 droplet</h1>
     </main>
   )
 }
