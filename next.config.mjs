dc0d9732a9c75b0465cb681a8c1ac8d9acf6ce4c
지윤 <1yoouoo@gmail.com>
2024-06-07 00:24:02 +0900

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['optimalprice.kr'],
+    domains: ['optimalprice.kr', 'cdn.optimalprice.kr'],
     remotePatterns: [
       {
         protocol: 'https',
