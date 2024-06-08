00d3e728c075ee030cc2774fa27f7be1969e3362
지윤 <1yoouoo@gmail.com>
2024-06-08 10:09:40 +0900

    test

--- app/(page)/components/Banner.tsx
+++ app/(page)/components/Banner.tsx
@@ -11,31 +11,35 @@ const Banner = () => {
   )
 
   return (
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={1}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-    >
-      {images.map((src, index) => (
-        <SwiperSlide key={index}>
-          <div className="relative w-full">
-            <Image
-              src={src}
-              alt={`Slide ${index + 1}`}
-              width={1280}
-              height={660}
-              objectFit="cover"
-              //   className="max-h-[660px] rounded-3xl"
-              unoptimized
-            />
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className="absolute left-0 w-screen bg-gray-400">
+      {/* 부모 요소의 마진을 제거하고 전체 너비 사용 */}
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1.2} // 슬라이드가 1.5개 보이도록 설정
+        centeredSlides={true} // 슬라이드를 가운데 정렬
+        slidesOffsetAfter={0}
+        slidesOffsetBefore={0}
+        navigation
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+      >
+        {images.map((src, index) => (
+          <SwiperSlide key={index}>
+            <div className="mx-auto h-[660px] w-[1280px]">
+              {/* overflow-hidden 추가 */}
+              <Image
+                src={src}
+                alt={`Slide ${index + 1}`}
+                width={1280}
+                height={660}
+                objectFit="cover"
+                className="rounded-3xl"
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   )
 }
 
