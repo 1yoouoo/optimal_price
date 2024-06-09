07a18d145d8bada0475c6de1d115ca6763356a9e
지윤 <1yoouoo@gmail.com>
2024-06-09 18:11:46 +0900

    tmp

--- app/provider/ToastProvider.tsx
+++ app/provider/ToastProvider.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { createContext, useState, ReactNode } from 'react'
+import { createContext, useState, ReactNode, useEffect } from 'react'
 import { Toast } from 'flowbite-react'
 import { HiCheck, HiExclamation, HiX } from 'react-icons/hi'
 
@@ -10,7 +10,6 @@ interface ToastMessage {
 }
 
 interface ToastContextType {
-  // eslint-disable-next-line no-unused-vars
   showToast: (toast: ToastMessage) => void
 }
 
@@ -20,56 +19,54 @@ export const ToastContext = createContext<ToastContextType | undefined>(
 
 export function ToastProvider({ children }: { children: ReactNode }) {
   const [toast, setToast] = useState<ToastMessage | null>(null)
+  const [fadeIn, setFadeIn] = useState(false)
+  const [fadeOut, setFadeOut] = useState(false)
 
   const showToast: ToastContextType['showToast'] = (toast) => {
     setToast(toast)
-    setTimeout(() => setToast(null), 3000) // 3초 후 토스트 메시지 사라짐
+    setFadeIn(true) // 즉시 페이드 인 시작
+    setFadeOut(false) // 페이드 아웃 초기화
+
+    setTimeout(() => {
+      setFadeIn(false)
+      setFadeOut(true)
+    }, 2000) // 2초 후 페이드 아웃 시작
   }
+
+  useEffect(() => {
+    if (fadeOut) {
+      const timer = setTimeout(() => {
+        setToast(null)
+        setFadeOut(false)
+      }, 500) // 0.5초 후 토스트 제거
+      return () => clearTimeout(timer)
+    }
+  }, [fadeOut])
 
   return (
     <ToastContext.Provider value={{ showToast }}>
       {children}
-      {toast?.type === 'success' && (
-        <span className="fixed bottom-10 left-1/2 z-50 flex -translate-x-1/2 transform flex-col gap-4">
+      <div
+        className={`fixed bottom-10 left-1/2 z-50 flex -translate-x-1/2 transform flex-col gap-4 transition-all duration-500 ease-in-out ${
+          fadeIn
+            ? 'translate-y-0 opacity-100'
+            : fadeOut
+              ? '-translate-y-10 opacity-0'
+              : 'translate-y-10 opacity-0'
+        }`}
+      >
+        {toast && (
           <Toast>
             <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg text-green-500">
-              <HiCheck className="h-5 w-5" />
+              {toast.type === 'success' && <HiCheck className="h-5 w-5" />}
+              {toast.type === 'error' && <HiExclamation className="h-5 w-5" />}
+              {toast.type === 'info' && <HiX className="h-5 w-5" />}
             </div>
-            <div className="ml-3 text-sm font-normal">
-              Item moved successfully.
-            </div>
+            <div className="ml-3 text-sm font-normal">{toast.message}</div>
             <Toast.Toggle />
           </Toast>
-        </span>
-      )}
-
-      {toast?.type === 'error' && (
-        <span className="fixed bottom-10 left-1/2 z-50 flex -translate-x-1/2 transform flex-col gap-4">
-          <Toast>
-            <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg text-green-500">
-              <HiCheck className="h-5 w-5" />
-            </div>
-            <div className="ml-3 text-sm font-normal">
-              Item moved successfully.
-            </div>
-            <Toast.Toggle />
-          </Toast>
-        </span>
-      )}
-
-      {toast?.type === 'info' && (
-        <span className="fixed bottom-10 left-1/2 z-50 flex -translate-x-1/2 transform flex-col gap-4">
-          <Toast>
-            <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg text-green-500">
-              <HiCheck className="h-5 w-5" />
-            </div>
-            <div className="ml-3 text-sm font-normal">
-              Item moved successfully.
-            </div>
-            <Toast.Toggle />
-          </Toast>
-        </span>
-      )}
+        )}
+      </div>
     </ToastContext.Provider>
   )
 }
