9a94dea301a9b72c8b51a13c0675df99f11da2fb
지윤 <1yoouoo@gmail.com>
2024-06-09 14:51:51 +0900

    베스트, 할인 페이지 추가

--- app/(page)/components/ItemList.tsx
+++ app/(page)/components/ItemList.tsx
@@ -12,13 +12,33 @@ interface ItemListProps {
 
 const cols = 5
 
-//TODO API요청시 limit있으면 효율적일 듯
+//TODO API 요청 시 limit있으면 효율적일 듯
 const ItemList = ({ title, endpoint, rows }: ItemListProps) => {
   const Items = getAppleProducts()
 
   console.log(endpoint)
 
-  if (!rows) return null //TODO 무한스크롤
+  //TODO 무한 스크롤
+  if (!rows)
+    return (
+      <div className="flex flex-col gap-10">
+        <div className="flex justify-between">
+          <Link href={endpoint}>
+            <h2 className="cursor-pointer text-3xl font-bold">{title}</h2>
+          </Link>
+          <Link href={endpoint}>
+            <span className="flex h-10 cursor-pointer items-center justify-center">
+              더보기
+            </span>
+          </Link>
+        </div>
+        <div className={`grid grid-cols-${cols} gap-x-4 gap-y-12`}>
+          {Items.map((item) => (
+            <CompactItem key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+    )
 
   const slicedItems = Items.slice(0, rows * cols)
 
--- app/(page)/discount/page.tsx
+++ app/(page)/discount/page.tsx
@@ -1,9 +1,11 @@
 import ItemList from '../components/ItemList'
 
-export default function Best() {
+const Discount = () => {
   return (
     <section className="mx-auto mb-10 mt-20 w-[1280px]">
-      <ItemList title="베스트" endpoint="/best" />
+      <ItemList title="할인율이 높은 상품" endpoint="/discount" />
     </section>
   )
 }
+
+export default Discount
